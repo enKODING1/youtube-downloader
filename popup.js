bd001efe389e59cdf418abc73d7db7ef4ad1f5ea
enkoding1 <ksjdevelop53@gmail.com>
2023-03-13 01:08:39 +0900

    Test: save and load

--- popup.js
+++ popup.js
@@ -1,9 +1,46 @@
+function renderItem(url, title) {
+  const html = `
+  <div class="item-wrap">
+  <p class="yt-url">
+      <a href="${url}">
+          ${title}
+      </a>
+  </p>
+</div>
+  `;
+
+  return html;
+}
+
+
 window.onload = () => {
   const add = document.querySelector(".add");
+  const content = document.querySelector(".content");
+
 
   add.onclick = () => {
+    console.log("click");
     chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
       chrome.tabs.sendMessage(tabs[0].id, { message: "add" });
     });
   };
+
+
+
+
+
+  chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
+   
+     
+      if(message === "error"){ return ; };
+
+      let stack =await chrome.storage.local.get("stack").then(result=>{return result.stack}) || [];
+      console.log(stack.length);
+      console.log('end');
+      
+    }
+  );
+
+
+
 };
